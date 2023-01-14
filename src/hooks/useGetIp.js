dcbd8480d21241de51c6b64081dfea12bacd4a65
Hank <77097180+ranjafunc@users.noreply.github.com>
2023-01-14 21:11:28 +0900

    fix: XMLHttpRequest undefiend 오류 수정

--- src/hooks/useGetIp.js
+++ src/hooks/useGetIp.js
@@ -3,12 +3,12 @@ import React, { useEffect, useState } from 'react';
 const useGetIp = () => {
     const [ip, setIp] = useState('');
 
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        const data = JSON.parse(xhr.responseText);
-        setIp(data.ip);
-    };
     const getIp = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            const data = JSON.parse(xhr.responseText);
+            setIp(data.ip);
+        };
         xhr.open('GET', 'https://api.ipify.org?format=json', true);
         xhr.send();
     };
