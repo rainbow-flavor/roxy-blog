41d425391ca74f71e471d20f2ea76ef13dadc12d
Hank <seolin9340@gmail.com>
2023-05-21 02:40:24 +0900

    config: whatap 스크립트 바디에 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,16 +2,20 @@ import React, { useEffect } from 'react';
 
 const App = ({ children }) => {
     useEffect(() => {
-        window.WhatapBrowserAgent = {
-            config: {
-                projectAccessKey: 'x4te823r4ri9p-x107ca46a3pivo-z16j3crpakvotc',
-                pcode: 30152,
-                sampleRate: 100,
-            },
-        };
+        const script = document.createElement('script');
+        script.innerHTML = `
+                window.WhatapBrowserAgent = {
+                    config: {
+                        projectAccessKey: 'x4te823r4ri9p-x107ca46a3pivo-z16j3crpakvotc',
+                        pcode: 30152,
+                        sampleRate: 100,
+                    },
+                };
+        `;
+        document.body.appendChild(script);
     }, []);
 
-    return children;
+    return <>{children}</>;
 };
 
 export default App;
